febb19a025a606adb0b9631c03575879f9beb333
Justin Litchfield <litch@me.com>
2015-02-28 11:24:13 -0600

    Exit out of the test after receiving 500 messages

--- consumer.rb
+++ consumer.rb
@@ -16,12 +16,13 @@ received = 0
 
 def do_work_that_blocks_the_event_loop(arg)
   p arg
+  Vertx.exit if arg >= 500
   sleep(0.1)
 end
 
-sub = eventstore.new_catchup_subscription(stream, -1)
-sub.on_event { |event| received += 1 ; do_work_that_blocks_the_event_loop(received)}
-sub.on_error { |error| p error.inspect }
-sub.start
-
-
+Vertx.set_timer(1_000) do
+  sub = eventstore.new_catchup_subscription(stream, -1)
+  sub.on_event { |event| received += 1 ; do_work_that_blocks_the_event_loop(received)}
+  sub.on_error { |error| p error.inspect }
+  sub.start
+end
