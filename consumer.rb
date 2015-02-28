05300e2f8d97af543326dee7577eb717d9d33777
Justin Litchfield <litch@me.com>
2015-02-28 11:29:12 -0600

    Remove sleep, restructure a bit

--- consumer.rb
+++ consumer.rb
@@ -17,12 +17,13 @@ received = 0
 def do_work_that_blocks_the_event_loop(arg)
   p arg
   Vertx.exit if arg >= 500
-  sleep(0.1)
 end
 
-Vertx.set_timer(1_000) do
-  sub = eventstore.new_catchup_subscription(stream, -1)
-  sub.on_event { |event| received += 1 ; do_work_that_blocks_the_event_loop(received)}
-  sub.on_error { |error| p error.inspect }
+sub = eventstore.new_catchup_subscription(stream, -1)
+sub.on_event { |event| received += 1 ; do_work_that_blocks_the_event_loop(received)}
+sub.on_error { |error| p error.inspect }
+
+Vertx.set_timer(5_000) do
+  p "Timeout reached, starting the subscriptions"
   sub.start
 end
